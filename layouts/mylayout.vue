6c542384efa7df708389d17dc421133217f948da
Pantakit-Joseph <mr.pantakit@gmail.com>
2021-06-21 16:47:14 +0700

    up003

--- layouts/mylayout.vue
+++ layouts/mylayout.vue
@@ -2,11 +2,12 @@
   <v-app dark>
     <div>
       <v-app-bar>
-        <v-btn icon>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+        <!-- <v-btn icon>
           <v-icon>
             mdi-menu
           </v-icon>
-        </v-btn>
+        </v-btn> -->
         <v-toolbar-title>Universal</v-toolbar-title>
 
         <v-spacer />
@@ -39,6 +40,50 @@
         </v-menu>
       </v-app-bar>
     </div>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list>
+        <v-avatar
+          size="96"
+        >
+          <v-img src="https://cdn.vuetifyjs.com/images/john.png" />
+        </v-avatar>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+              John Leider
+            </v-list-item-title>
+            <v-list-item-subtitle>john@vuetifyjs.com</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+
+      <v-divider />
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <v-container>
@@ -54,7 +99,18 @@ export default {
     app_bar_items: [
       { title: 'Settings' },
       { title: 'Favorites' }
-    ]
-  })
+    ],
+    items: [
+      { title: 'Home', icon: 'mdi-view-dashboard' },
+      { title: 'About', icon: 'mdi-forum' }
+    ],
+    drawer: false,
+    group: null
+  }),
+  watch: {
+    group () {
+      this.drawer = false
+    }
+  }
 }
 </script>
