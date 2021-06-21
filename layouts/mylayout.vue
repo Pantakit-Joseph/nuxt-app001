9d18399d370ee4867fa7739ff3d3fbe7388660bd
Pantakit-Joseph <mr.pantakit@gmail.com>
2021-06-21 15:25:10 +0700

    up001

---
+++ layouts/mylayout.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app dark>
+    <div>
+      <v-app-bar>
+        <v-btn icon>
+          <v-icon>
+            mdi-menu
+          </v-icon>
+        </v-btn>
+        <v-toolbar-title>Universal</v-toolbar-title>
+
+        <v-spacer />
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-menu
+          bottom
+        >
+          <template #activator="{ on, attrs }">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item
+              v-for="(item, i) in app_bar_items"
+              :key="i"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+    </div>
+
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    app_bar_items: [
+      { title: 'Settings' },
+      { title: 'Favorites' }
+    ]
+  })
+}
+</script>
